a2d0d032d0061d00d8d2531baa824acda71cfcf3
Damir Pavlik <damir@omnistreak.com>
2025-05-27 23:17:04 +0200

    build: add packer

---
+++ lua/damir/packer.lua
@@ -0,0 +1,43 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	})
+
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use('nvim-treesitter/playground')
+	use {
+	    "ThePrimeagen/harpoon",
+	    branch = "harpoon2",
+	    requires = { {"nvim-lua/plenary.nvim"} }
+	}
+	use('mbbill/undotree')
+	use('tpope/vim-fugitive')
+
+	use { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' }
+	use { 'williamboman/mason.nvim' }
+	use { 'williamboman/mason-lspconfig.nvim' }
+	use { 'neovim/nvim-lspconfig' }
+	use { 'hrsh7th/nvim-cmp' }
+	use { 'hrsh7th/cmp-nvim-lsp' }
+	use { 'L3MON4D3/LuaSnip' }
+	use { 'saadparwaiz1/cmp_luasnip' }
+	use { 'hrsh7th/cmp-buffer' }
+	use { 'hrsh7th/cmp-path' }
+end)
