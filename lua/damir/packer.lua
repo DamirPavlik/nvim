d6d81b4fce2427d36a06efacc98604b81b3934b4
Damir Pavlik <damir@omnistreak.com>
2025-06-14 14:52:55 +0200

    feat: add snippets

--- lua/damir/packer.lua
+++ lua/damir/packer.lua
@@ -34,9 +34,9 @@ return require('packer').startup(function(use)
 	use { 'williamboman/mason.nvim' }
 	use { 'williamboman/mason-lspconfig.nvim' }
 	use { 'neovim/nvim-lspconfig' }
-	use { 'hrsh7th/nvim-cmp' }
+	-- use { 'hrsh7th/nvim-cmp' }
 	use { 'hrsh7th/cmp-nvim-lsp' }
-	use { 'L3MON4D3/LuaSnip' }
+	-- use { 'L3MON4D3/LuaSnip' }
 	use { 'saadparwaiz1/cmp_luasnip' }
 	use { 'hrsh7th/cmp-buffer' }
 	use { 'hrsh7th/cmp-path' }
@@ -46,5 +46,71 @@ return require('packer').startup(function(use)
       config = function()
         require('Comment').setup()
       end
+    }
+
+    use {
+        'windwp/nvim-ts-autotag',
+        config = function()
+            require('nvim-ts-autotag').setup()
+        end,
+        ft = { 'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' }
+    }
+
+    use {
+      'L3MON4D3/LuaSnip',
+      dependencies = { 'rafamadriz/friendly-snippets' },
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end
+    }
+
+    use {
+        'hrsh7th/nvim-cmp',
+        requires = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'saadparwaiz1/cmp_luasnip',
+            'L3MON4D3/LuaSnip'
+        },
+        config = function()
+            local cmp = require'cmp'
+            local luasnip = require'luasnip'
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                })
+            })
+        end
     }
 end)
